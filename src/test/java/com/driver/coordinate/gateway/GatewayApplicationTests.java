83049e7c7b15f5ec9051f6470fae26c179439753
Daniel Lopes <daniel.lpsq@gmail.com>
2023-07-23 18:20:28 -0300

    first commit

---
+++ src/test/java/com/driver/coordinate/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.driver.coordinate.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
